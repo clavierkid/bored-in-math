e6fb4f88bf71b72339cb8c4d7d9c4079a9a660f5
clavierkid <carter@MacBook-Air.local>
2017-01-26 20:00:44 -0700

    It actually works
    for real this time. it works. again.

--- Matrices/MatrixCode.py
+++ Matrices/MatrixCode.py
@@ -1,5 +1,7 @@
+#!/usr/local/bin/python3
 import numpy as np
 import numpy.linalg
+import math
 alphabet = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 mode = input("Encode or decode? (e/d)")
@@ -16,15 +18,13 @@ print("[a b c\nd e f\ng h i]")
 encodeInput = input("Please insert your 3x3 encoding matrix in form (a b c; d e f; g h i):")
 encodeMatrix = np.matrix(encodeInput)
 decodeMatrix = numpy.linalg.inv(encodeMatrix)
-print(encodeMatrix)
-print(decodeMatrix)
+print("inverse matrix:\n",decodeMatrix)
 
 if decode == False:
   dMessage = input("Enter your plaintext message (letters and spaces only): ")
 elif decode == True:
   dMessage = input("Enter your encoded text: ")
 dMessage = dMessage.lower()
-
 encoded = []
 for i in range(len(dMessage)):
   encoded.append(alphabet.index(dMessage[i]))
@@ -36,13 +36,11 @@ elif length%3 == 2:
   encoded.append(0)
   encoded.append(0)
 
-# print("Before:",encoded)
 encoded[0] = [encoded[0],encoded[3]]
 encoded[1] = [encoded[1],encoded[4]]
 encoded[2] = [encoded[2],encoded[5]] 
 shortLength = int(len(encoded)/3)
 steps = 0
-# print("\n\nMiddle:",encoded)
 for i in range(1,shortLength-1):
   steps = steps + 1
   encoded[0].append(encoded[3*i+3])
@@ -50,20 +48,29 @@ for i in range(1,shortLength-1):
   encoded[2].append(encoded[3*i+5])
 del(encoded[3:])
 
-# print("\n\nAfter:",encoded)
 encoded = np.matrix(encoded)
-final = decodeMatrix * encoded
+if decode == True:
+  final = decodeMatrix * encoded
+elif decode == False:
+  final = encodeMatrix * encoded
 final = final%27
 
 print("Your original matrix:\n",encoded)
-print("Your decoded matrix:\n",final)
+if decode == True:
+  print("Your decoded matrix:\n",final)
+elif decode == False:
+  print("Your encoded matrix:\n", final)
 
 message = []
 final = final.tolist()
 
 for j in range(len(final[0])):
   for i in range(len(final)):
-    final[i][j] = int(final[i][j])
+    print(i,j,final[i][j],message,"\n")
+    final[i][j] = round(final[i][j])
     message.append(alphabet[final[i][j]])
 
-print("Your final decoded message is:","".join(message))
+if decode == True:
+  print("Your final decoded message is:","".join(message))
+elif decode == False:
+  print("Your final encoded message is:","".join(message))
